1c7e36b8f169021a103767ee1e31db05e0173121
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-06-12 12:09:09 +0200

    Uitwerking van de exercise

---
+++ email-check.php
@@ -0,0 +1,13 @@
+<?php
+$pattern = '/[\w]{3,64}[@]{1}[\w]{3,255}[.]{1}[a-z]{3}/m';
+
+$input = readline("email pls");
+
+if (preg_match($pattern, $input))
+{
+    echo("$input is een geldig email address");
+}
+else
+{
+    echo("$input is niet een geldig email address");
+}
