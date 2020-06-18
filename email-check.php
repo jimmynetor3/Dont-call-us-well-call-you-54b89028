3a054e9d194b66de3fe3c7c07178bf623e4edabb
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-06-18 10:57:57 +0200

    f blast

--- email-check.php
+++ email-check.php
@@ -1,13 +1,11 @@
 <?php
+
 $pattern = '/[\w]{3,64}[@]{1}[\w]{3,255}[.]{1}[a-z]{2,3}/m';
 
 $input = $argv[1];
 
-if (preg_match($pattern, $input))
-{
+if (preg_match($pattern, $input)) {
     echo("$input is een geldig email address");
-}
-else
-{
+} else {
     echo("$input is niet een geldig email address");
 }
